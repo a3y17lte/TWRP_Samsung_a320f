e396cdad6b7bfa25ce1e6eec568388aa557ca405
McFy49 <31405855+McFy49@users.noreply.github.com>
2018-02-05 21:36:20 +0100

    Up to omni path

--- omni_a3y17lte.mk
+++ omni_a3y17lte.mk
@@ -5,7 +5,7 @@ PRODUCT_RELEASE_NAME := a3y17lte
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_a3y17lte
